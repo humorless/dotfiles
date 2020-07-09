847a140a17608286889e7c7dedf678abec4b857d
Laurence Chen <humorless@gmail.com>
2020-07-09 14:42:56 +0800

    add cider-nrepl plugin for Neovim + Conjure

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,11 +5,11 @@
                   [jonase/eastwood "0.2.5"]
                   [lein-marginalia "0.9.1"]
                   [io.taylorwood/lein-native-image "0.3.0"]
-                  [lein-count "1.0.9"]]
+                  [lein-count "1.0.9"]
+                  [[cider/cider-nrepl "0.25.2"]]]
         :injections [(require 'vinyasa.inject)
                      (vinyasa.inject/inject 'clojure.core '>
-                       '[[vinyasa.inject inject]
-                         [vinyasa.pull pull]
-                         [clojure.repl doc source]
-                         [clojure.pprint pprint pp]])]
-        }}
+                                            '[[vinyasa.inject inject]
+                                              [vinyasa.pull pull]
+                                              [clojure.repl doc source]
+                                              [clojure.pprint pprint pp]])]}}
