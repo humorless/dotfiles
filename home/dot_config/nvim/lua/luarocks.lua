68a0f00156b349dbdd5dc08a9d228b87309d502e
Laurence Chen <laurence@replware.dev>
2025-09-05 15:07:36 +0800

    update lua/luarocks.lua

--- home/dot_config/nvim/lua/luarocks.lua
+++ home/dot_config/nvim/lua/luarocks.lua
@@ -1,10 +1,9 @@
 local function add_luarocks_paths()
-  local luarocks_path = "/Users/laurencechen/.luarocks-luajit/share/lua/5.1/?.lua;/Users/laurencechen/.luarocks-luajit/share/lua/5.1/?/init.lua"
-  local luarocks_cpath = "/Users/laurencechen/.luarocks-luajit/lib/lua/5.1/?.so"
-
+  local home_dir = os.getenv("HOME")
+  local luarocks_path = string.format("%s/.luarocks-luajit/share/lua/5.1/?.lua;%s/.luarocks-luajit/share/lua/5.1/?/init.lua", home_dir, home_dir)
+  local luarocks_cpath = string.format("%s/.luarocks-luajit/lib/lua/5.1/?.so", home_dir)
   package.path = package.path .. ";" .. luarocks_path
   package.cpath = package.cpath .. ";" .. luarocks_cpath
 end
 
 return {add_luarocks_paths = add_luarocks_paths}
-
